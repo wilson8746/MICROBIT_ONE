5d661c1711920895299cd962f240f9137c2b87b4
WILSON RINCON SUAREZ <113402669+wilson8746@users.noreply.github.com>
2022-10-24 16:13:29 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(9)
+    basic.pause(1000)
+    basic.showNumber(5)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(1000)
+})
 basic.forever(function () {
 	
 })
